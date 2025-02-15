f90b278a36b37278c7b242d7b118fe9e271528ff
madammelon20 <madammelon20@gmail.com>
2025-02-15 22:53:31 +0200

    Микросервисная система. Задача 4.1

--- backend-resources/src/test/java/com/itm/space/backendresources/BaseIntegrationTest.java
+++ backend-resources/src/test/java/com/itm/space/backendresources/BaseIntegrationTest.java
@@ -1,23 +1,14 @@
 package com.itm.space.backendresources;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.File;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @SpringBootTest
 @AutoConfigureMockMvc
